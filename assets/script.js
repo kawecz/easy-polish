9966c8339d5e6ea1a6a7c4f1d4de7f1850593431
Kawê Cezar <kawecezarjesus@gmail.com>
2024-11-01 17:44:04 -0500

    Finishing (i hope) the burger

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector(".nav-list")
+const nav = document.querySelector(".nav")
 const burger = document.querySelector(".burger")
 
 burger.addEventListener("click", () => nav.classList.toggle("is-active"));
